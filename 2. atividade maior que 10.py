0e05bf864b009cb0e47804e43066dff6b9808c89
Matheus-bahia21 <matheusgrego535@gmail.com>
2025-02-19 18:11:45 +0000

    atividade 2

---
+++ 2. atividade maior que 10.py
@@ -0,0 +1,19 @@
+# limpar o terminal
+import os 
+os.system("clear") 
+
+
+
+
+
+
+
+
+numero = int(input("digite um número: "))
+
+if numero == 10: 
+    print("é igual a 10!")
+elif numero > 10:
+    print ("é maior que 10!")  
+else: 
+    print ("não é maior que 10!")      
