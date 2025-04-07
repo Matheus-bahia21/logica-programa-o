82185340dcceaa02bc3b003ce878a22cb35a5d99
Matheus-bahia21 <matheusgrego535@gmail.com>
2025-04-07 19:24:35 +0000

    atividade

--- funções sem retorno. exemplos  /10.atividade.py
+++ funções sem retorno. exemplos  /10.atividade.py
@@ -4,19 +4,22 @@ os.system("clear || cls ")
 
 
 def calcular_media(nota1, nota2):
-    return (nota1 + nota2) / 2
+    soma = nota1 + nota2
+    resultado = soma / 2 
+    return resultado 
 
-def verificar_aprovacao(media):
+def verificar_resultado(media):
     if media >= 7:
-        return "Aluno aprovado."
+        print("Aluno aprovado.")
     else:
-        return "Aluno reprovado."
+        print("Aluno reprovado.")
 
+print("= NOTAS =")
 nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
 
 media = calcular_media(nota1, nota2)
-resultado = verificar_aprovacao(media)
+resultado = verificar_resultado(media)
 
 print(f"Média: {media:.2f}")
 print(resultado)
