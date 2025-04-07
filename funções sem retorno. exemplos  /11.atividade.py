1c027f449dad1989b3301927a238dbc8628b96be
Matheus-bahia21 <matheusgrego535@gmail.com>
2025-04-07 19:29:40 +0000

    atividade

--- funções sem retorno. exemplos  /11.atividade.py
+++ funções sem retorno. exemplos  /11.atividade.py
@@ -1,3 +1,12 @@
 import os 
 
 os.system("clear || cls ")
+
+def converter_para_centimetros(metros):
+    return metros * 100
+
+
+metros = float(input("Digite um valor em metros: "))
+centimetros = converter_para_centimetros(metros)
+
+print(f"{metros} metros equivalem a {centimetros} centímetros.")
