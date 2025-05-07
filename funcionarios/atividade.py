f80f5f00f6057395a4ed4cce082432cdc799a67f
Matheus-bahia21 <matheusgrego535@gmail.com>
2025-05-07 18:49:24 +0000

    atividade

---
+++ funcionarios/atividade.py
@@ -0,0 +1,34 @@
+import os
+from dataclasses import dataclass
+os.system("clear")
+
+@dataclass
+class funcionarios:
+    # Atributos: sao variaveis que pertencem a classe.
+    nome: str
+    data_nascimento: str
+    RG: int
+    CPF: int
+
+    # Metodo: e uma funçao que pertence a classe.
+    # Este metodo para exibir dados do paciente.
+    def exibir_dados(self):
+        print(f"Nome: {self.nome} \n data_nascimento: {self.data_nascimento} \n RG: {self.RG}\n CPF: {self.CPF}")
+
+lista_de_funcionarios = []
+QUANTIDADE_FUNCIONARIOS= 5
+
+for i in range (QUANTIDADE_FUNCIONARIOS):
+    funcionarios = funcionarios(
+                    nome = input("Digite o nome do funcionario: "),
+                    data_nascimento = float(input("Digite a data de nascimento: ")),
+                    RG = int(input("Digite o numero do RG: ")),
+                    CPF = int(input("Digite o numero do CPF: "))
+            )
+    lista_de_funcionarios.append(funcionarios)
+    print()
+
+nome_arquivo = "catalogo_funcionarios.txt"
+with open(nome_arquivo, "a") as arquivo_livros:
+    for funcionarios in lista_de_funcionarios:
+        arquivo_livros.write(f"{funcionarios.nome}, {funcionarios.data_nascimento}, {funcionarios.RG}, {funcionarios.CPF} \n")
